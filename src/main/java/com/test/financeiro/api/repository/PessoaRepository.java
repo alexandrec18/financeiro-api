947ad961b6710cb91fabf72ca333784101ec4b46
Alexandre Carbono <carbono@zapalago.com>
2018-09-22 08:07:58 -0300

    Ultimos ajustes api

--- src/main/java/com/test/financeiro/api/repository/PessoaRepository.java
+++ src/main/java/com/test/financeiro/api/repository/PessoaRepository.java
@@ -1,10 +1,14 @@
 package com.test.financeiro.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.test.financeiro.api.entities.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+	
+	Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 	
 
 }
